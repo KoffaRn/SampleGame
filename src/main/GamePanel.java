c8f7ffab9c80be14087994cd9884246adf2c25c5
Kristoffer Larsson <lilamoln.koffa@gmail.com>
2023-02-10 09:37:27 +0100

    Added TileManager, Entity manager, Player class

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,6 +1,7 @@
 package main;
 
 import entity.Player;
+import objects.SuperObject;
 import tile.Tile;
 import tile.TileManager;
 
@@ -32,7 +33,9 @@ public class GamePanel extends JPanel implements Runnable {
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
     public CollisionChecker cChecker = new CollisionChecker(this);
+    public AssetSetter aSetter = new AssetSetter(this);
     public Player player = new Player(this,keyH);
+    public SuperObject obj[] = new SuperObject[10];
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
@@ -43,6 +46,9 @@ public class GamePanel extends JPanel implements Runnable {
 
     }
 
+    public void setupGame() {
+        aSetter.setObject();
+    }
     public void startGameThread() {
         gameThread = new Thread(this);
         gameThread.start();
@@ -77,8 +83,19 @@ public class GamePanel extends JPanel implements Runnable {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D)g;
+        // TILE
         tileM.draw(g2);
+
+        //OBJECT
+        for(int i = 0; i < obj.length; i++) {
+            if(obj[i] != null) {
+                obj[i].draw(g2, this);
+            }
+        }
+
+        // PLAYER
         player.draw(g2);
+
         g2.dispose();
     }
 }
